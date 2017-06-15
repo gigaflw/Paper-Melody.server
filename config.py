9fa6d6024efe81301e7a7b8f49ffff568e2f1fbe
hgs1217 <mf20080540@126.com>
2017-06-15 10:43:45 +0800

    add /root

--- config.py
+++ config.py
@@ -20,5 +20,5 @@ if platform.system() == "Windows":  # 本地测试使用的文件夹地址
     UPLOAD_FOLDER = 'D:/Computer Science/Github/Paper-Melody.server/uploads/file' 
     UPLOAD_IMAGE_FOLDER = 'D:/Computer Science/Github/Paper-Melody.server/uploads/image' 
 else:
-    UPLOAD_IMAGE_FOLDER = '/Paper-Melody.server/uploads/image'  #服务器使用的文件夹地址
-    UPLOAD_FOLDER = '/Paper-Melody.server/uploads/file'  #服务器使用的文件夹地址
+    UPLOAD_IMAGE_FOLDER = '/root/Paper-Melody.server/uploads/image'  #服务器使用的文件夹地址
+    UPLOAD_FOLDER = '/root/Paper-Melody.server/uploads/file'  #服务器使用的文件夹地址
